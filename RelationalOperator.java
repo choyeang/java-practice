755f129e8bbb4d08fc9f103913d8cbdfff2c7e75
choyeang <choyeang0714@gmail.com>
2023-03-16 19:27:29 +0900

    no message

---
+++ RelationalOperator.java
@@ -0,0 +1,16 @@
+public class RelationalOperator{
+	public static void main(String[]args) {
+		//演示关系运算符
+		int a = 9;
+		int b = 8;
+		System.out.println(a > b);
+		System.out.println(a < b);
+		System.out.println(a >= b);
+		System.out.println(a <= b);
+		System.out.println(a == b);
+		System.out.println(a != b);
+		boolean flag = a > b;
+		System.out.println("flag ="+ flag);
+		
+	}
+}
